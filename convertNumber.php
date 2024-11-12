a886df548159ee7d0b972a0f24ca241cd236b875
huudungg <huudung2004kt@gmail.com>
2024-11-12 08:13:36 +0700

    final exercise

--- convertNumber.php
+++ convertNumber.php
@@ -1,43 +1,45 @@
 <?php
 $yourNumber = $_POST["yourNumber"];
-$textOfNumber = '';
-
-if ($yourNumber < 10 && $yourNumber >= 0) {
-    switch ($yourNumber) {
-        case 0:
-            $textOfNumber = "Zero";
-            break;
-        case 1:
-            $textOfNumber = "One";
-            break;
-        case 2:
-            $textOfNumber = "Two";
-            break;
-        case 3:
-            $textOfNumber = "Three";
-            break;
-        case 4:
-            $textOfNumber = "Four";
-            break;
-        case 5:
-            $textOfNumber = "Five";
-            break;
-        case 6:
-            $textOfNumber = "Six";
-            break;
-        case 7:
-            $textOfNumber = "Seven";
-            break;
-        case 8:
-            $textOfNumber = "Eight";
-            break;
-        case 9:
-            $textOfNumber = "Nine";
-            break;
+function readNumber($number)
+{
+    if ($number < 0 || $number > 999) {
+        return "out of ability";
     }
-} else {
-    $textOfNumber = "Out of the ability";
+
+    $ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    $teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    $tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    $result = "";
+
+    // Xử lý hàng trăm
+    if ($number >= 100) {
+        $hundreds = intval($number / 100);
+        $result .= $ones[$hundreds] . " hundred";
+        $number %= 100;
+        if ($number > 0) {
+            $result .= " and ";
+        }
+    }
+
+    // Xử lý hàng chục và hàng đơn vị
+    if ($number >= 20) {
+        $tens_place = intval($number / 10);
+        $result .= $tens[$tens_place];
+        $number %= 10;
+        if ($number > 0) {
+            $result .= " " . $ones[$number];
+        }
+    } elseif ($number >= 10) {
+        $result .= $teens[$number - 10];
+    } elseif ($number > 0) {
+        $result .= $ones[$number];
+    }
+
+    return $result;
 }
+
+
 
 ?>
 <!DOCTYPE html>
@@ -48,7 +50,7 @@ if ($yourNumber < 10 && $yourNumber >= 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-Convert to text: <?php echo $textOfNumber ?>
+Convert to text => <?php echo readNumber($yourNumber); ?>
 
 <body>
 
