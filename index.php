bf05d2dcd13375c7da57225d7acecb79a8c6e58e
huudungg <huudung2004kt@gmail.com>
2024-11-12 08:24:28 +0700

    upload file

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<?php
-$yourNumber = 100;
+<?php date_default_timezone_set('Asia/Ho_Chi_Minh') ?>
+<html>
 
-function isPrime($num)
-{
-    for ($i = 2; $i < $num; $i++) {
-        if ($num % $i == 0) {
-        } else {
-            echo $i . ' is prime';
-        }
-    }
-}
+<head>
+    <title>Thời gian hiện tại</title>
+</head>
 
+<body>
+    <h1> Bây giờ là: <?php echo date('Y-M-d h:m:s'); ?> </h1>
+</body>
 
-isPrime($yourNumber);
+</html>
