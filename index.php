9cdc25aecf466d49798c6e0298c58e89c9fec4ce
huudungg <huudung2004kt@gmail.com>
2024-11-12 08:35:52 +0700

    upload code

--- index.php
+++ index.php
@@ -1,79 +1,60 @@
-<?php
-$customerlist = array(
-    "1" => array(
-        "ten" => "Mai Văn Hoàn",
-        "ngaysinh" => "1983-08-20",
-        "diachi" => "Hà Nội",
-        "anh" => "images/img1.jpg"
-    ),
-    "2" => array(
-        "ten" => "Nguyễn Văn Nam",
-        "ngaysinh" => "1983-08-20",
-        "diachi" => "Bắc Giang",
-        "anh" => "images/img2.jpg"
-    ),
-    "3" => array(
-        "ten" => "Nguyễn Thái Hòa",
-        "ngaysinh" => "1983-08-21",
-        "diachi" => "Nam Định",
-        "anh" => "images/img3.jpg"
-    ),
-    "4" => array(
-        "ten" => "Trần Đăng Khoa",
-        "ngaysinh" => "1983-08-22",
-        "diachi" => "Hà Tây",
-        "anh" => "images/img4.jpg"
-    ),
-    "5" => array(
-        "ten" => "Nguyễn Đình Thi",
-        "ngaysinh" => "1983-08-17",
-        "diachi" => "Hà Nội",
-        "anh" => "images/img5.jpg"
-    )
-);
-?>
-
 <!DOCTYPE html>
 <html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
 
-    th,
-    td {
-        padding: 8px;
-        text-align: left;
-        border-bottom: 1px solid #ddd;
-    }
-</style>
+<head>
+    <meta charset="UTF-8">
+    <title>Từ Điển Anh - Việt</title>
+    <style>
+        input[type=text] {
+            width: 300px;
+            font-size: 16px;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            padding: 12px 10px 12px 10px;
+        }
+
+        #submit {
+            border-radius: 2px;
+            padding: 10px 32px;
+            font-size: 16px;
+        }
+    </style>
+</head>
 
 <body>
-    <table>
-        <caption>
-            <h1>Danh sách khách hàng</h1>
-        </caption>
-        <tr>
-            <th>STT</th>
-            <th>Tên</th>
-            <th>Ngày sinh</th>
-            <th>Địa chỉ</th>
-            <th>Ảnh</th>
-        </tr>
-        <?php
-        foreach ($customerlist as $key => $values) {
-            echo "<tr>";
-            echo "<td>" . $key . "</td>";
-            echo "<td>" . $values['ten'] . "</td>";
-            echo "<td>" . $values['ngaysinh'] . "</td>";
-            echo "<td>" . $values['diachi'] . "</td>";
-            echo "<td><image src ='" . $values['anh'] . "' width = '60px' height ='60px'/></td>";
-            echo "</tr>";
+    <h2>Từ Điển Anh - Việt</h2>
+    <form method="post">
+        <input type="text" name="search" placeholder="Nhập từ cần tìm" />
+        <input type="submit" id="submit" value="Tìm" />
+    </form>
+
+    <?php
+    $dictionary = array(
+        "hello" => "xin chào",
+        "how" => "thế nào",
+        "book" => "quyển vở",
+        "computer" => "máy tính"
+    );
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $searchword = $_POST["search"];
+        $flag = 0;
+
+        // Duyệt mảng và kiểm tra từ
+        foreach ($dictionary as $word => $description) {
+            if ($word == $searchword) {
+                echo "Từ: " . $word . "<br/>Nghĩa của từ: " . $description;
+                $flag = 1;
+                break;
+            }
         }
-        ?>
-    </table>
+
+        // Nếu không tìm thấy từ
+        if ($flag == 0) {
+            echo "Không tìm thấy từ cần tra.";
+        }
+    }
+    ?>
 </body>
 
 </html>
